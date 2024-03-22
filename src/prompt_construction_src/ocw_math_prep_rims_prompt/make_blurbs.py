f02373f512c35fe82161830a046959061d8f2862
freeman.genie777@gmail.com <sonsus@github.com>
2024-03-23 04:00:07 +0900

    rims prompts + ablation prompts

--- src/prompt_construction_src/ocw_math_prep_rims_prompt/make_blurbs.py
+++ src/prompt_construction_src/ocw_math_prep_rims_prompt/make_blurbs.py
@@ -2,15 +2,16 @@ import yaml
 from typing import List, Literal
 from itertools import permutations
 import os
-
+from collections import defaultdict
 
 # from pqdm.processes import pqdm
 from tqdm import tqdm
 from fire import Fire
 from munch import Munch, munchify
-from openai import AzureOpenAI
+from openai import AsyncAzureOpenAI
+import asyncio
 
-client = AzureOpenAI(
+client = AsyncAzureOpenAI(
         azure_endpoint=os.getenv("OLD_AZURE_OPENAI_ENDPOINT"),
         api_key=os.getenv("OLD_AZURE_OPENAI_API_KEY"),
         api_version="2023-07-01-preview",
@@ -18,19 +19,25 @@ client = AzureOpenAI(
         timeout=120,   
     )
 
-def query_openai(
-        prompt:List[str], 
-        model:str="GPT4-1106")->List[str]:
+semaphore = asyncio.Semaphore(6)
 
-    response = client.chat.completions.create(
-        temperature = 0.9,
-        model = model,
-        messages = prompt,
-        seed = 777,
-        n = 2,
-        max_tokens = 1024,
-        stop = "`Workaround Method`:",
-    )
+def flatten(l:List[List[str]])->List[str]:
+    return [item for sublist in l for item in sublist]
+
+async def query_openai(
+        prompt:List[str], 
+        model: str = "GPT4-1106")->List[str]:
+        # model: str = "GPT-35")->List[str]:
+    async with semaphore:
+        response = await client.chat.completions.create(
+            temperature = 0.9,
+            model = model,
+            messages = prompt,
+            seed = 777,
+            n = 2,
+            max_tokens = 1024,
+            stop = "`Workaround Method`:",
+        )
     return [response.choices[i].message.content for i in range(len(response.choices))]
 
 def get_prompt(
@@ -61,34 +68,48 @@ def get_prompt(
         {"role": "system", "content": system},
     ] 
     # content
-    content = template.user
+    blurb_placeholder = template.user
     to_replace_map = {
-        "QUESTION": question,
+        "QUESTION": question.strip(),
         "WRONG_METHOD": _abb2full(wrong_method),
-        "WRONG_SOLUTION": wrong_solution,
-        "WRONG_PRED": wrong_pred, 
+        "WRONG_SOLUTION": wrong_solution.strip(),
+        "WRONG_PRED": wrong_pred.strip(), 
         "CORRECT_METHOD": _abb2full(correct_method),
-        "CORRECT_SOLUTION": correct_solution,
-        "CORRECT_PRED": correct_pred,
+        "CORRECT_SOLUTION": correct_solution.strip(),
+        "CORRECT_PRED": correct_pred.strip(),
     }
     for key, value in to_replace_map.items():
         key_ = f"{{{key}}}"
-        content = content.replace(key_, str(value))
-    # print(content)
-    prompt.append({"role": "user", "content": content})
+        blurb_placeholder = blurb_placeholder.replace(key_, str(value))
+    prompt.append({"role": "user", "content": blurb_placeholder})
 
-    return prompt
+    return prompt, blurb_placeholder
     
 
-def prep_blurbs(
+async def prep_blurbs(
         src_d: dict, 
         wrong_method: Literal["cot", "pal", "p2c"] = None, 
-        correct_method: Literal["cot", "pal", "p2c"] = None
+        correct_method: Literal["cot", "pal", "p2c"] = None,
+        progress_bar: tqdm = None,
         ) -> List[str]:
     """
     returns (question * 2) blurbs 
     """    
     
+    def _generated2blanks(blurb_w_placeholder:str, bl:str):
+        try:
+            mistake, hint = [l for l in bl.split("\n") if l.strip()]
+            mistake = mistake.replace("`Mistakes`: ", "")
+            hint = hint.replace("`Hint for a better Method choice`: ", "")
+        except Exception as e:
+            mistake, hint = f"ERROR: {bl.strip()}", str(e)
+        mistake_placeholder = "<one_liner_explanation_for_whats_gone_wrong_in_the_attempt>"
+        hint_placeholder = "<one_liner_hint_to_workaround_with_different_method>"
+
+        filled = blurb_w_placeholder.replace(mistake_placeholder, mistake).replace(hint_placeholder, hint)
+        return filled
+        
+
     # extract needs for the prompt:
     # question, wrong_solution, wrong_pred, correct_solution, correct_pred
     all_blurbs = []
@@ -97,7 +118,7 @@ def prep_blurbs(
         wrong_pred = src_d.wrong.preds[wrong_method][i]
         correct_solution = src_d.correct[correct_method][i]
         correct_pred = src_d.answers[i]
-        prompt = get_prompt(
+        prompt, blurb_w_placeholder = get_prompt(
             question = question,
             wrong_method = wrong_method,
             wrong_solution = wrong_solution,
@@ -106,48 +127,74 @@ def prep_blurbs(
             correct_solution = correct_solution,
             correct_pred = correct_pred
         )
-        two_blurbs = query_openai(prompt)
-        print(two_blurbs[0])
-        # print(two_blurbs[1])
+        two_blurbs_ = await query_openai(prompt)
+        two_blurbs = [_generated2blanks(blurb_w_placeholder, bl) for bl in two_blurbs_]
+
         numbered_blurbs = [f"{i}\n{bl.strip()}" for bl in two_blurbs]
+        progress_bar.update(2)
         all_blurbs.extend(numbered_blurbs)
 
     return all_blurbs
 
 
-def main():
+async def main():
     hint_template: dict = yaml.full_load(open("make_blurbs_template.yaml"))
     fewshots: dict = yaml.full_load(open("make_blurbs_resources.yaml"))
+    # fewshots: dict = yaml.full_load(open("make_blurbs_resources_pal_p2c_aug.yaml"))
 
     # make it attribute-accessible
     hint_template = munchify(hint_template)
     fewshots = munchify(fewshots)
 
     # make the prompt
+    # datasets = ["math"]
     datasets = ["ocw", "math"]
-    methods = "cot pal p2c".split()
     
+    # methods = "cot pal p2c".split()
     # wrong2correct_directions = permutations(methods, 2)
     wrong2correct_directions = [
-        "p2c-cot", # 1
-        "pal-p2c", # 3 
-        "pal-cot", # 3
-        "cot-p2c", # 2
+        "p2c-cot", 
+        "pal-p2c",  
+        "pal-cot", 
+        "cot-p2c", 
     ]
+    awaitables = []
+    keys = []
+    pbar = tqdm(
+        range(
+            len(datasets)* \
+            len(wrong2correct_directions)* \
+            len(fewshots.math.questions) * 2
+            )
+    )
     for ds in datasets:
-        src_d = fewshots[ds]
-        for wrong_correct in tqdm(wrong2correct_directions):
+        for wrong_correct in wrong2correct_directions:
             w_method, c_method = wrong_correct.split("-")
             
             # make blurbs
-            blurbs = prep_blurbs(src_d, wrong_method = w_method, correct_method = c_method)
+            task = asyncio.create_task(prep_blurbs(fewshots[ds], wrong_method = w_method, correct_method = c_method, progress_bar=pbar))
+            awaitables.append(task)
             
-            # write to txt
+            # annotate jobs
             outf = f"blurbs_{ds}_{wrong_correct}.txt"
-            with open(outf, "w") as wf:
-                sep = f"\n\n\n\n{'='*20}\n\n\n\n"
-                for txt in blurbs:
-                    wf.write(txt + sep)
+            # outf = f"blurbs_{ds}_{wrong_correct}_aug.txt"
+            keys.append(outf)
+            
+    # start jobs
+    done = await asyncio.gather(*awaitables)
+
+    
+    # pair the jobs
+    fname2tasks = defaultdict(list)
+    for key, tasks in zip(keys, done):
+        fname2tasks[key].extend(tasks)
+
+    # write to txt
+    for outf, completed in fname2tasks.items():
+        with open(outf, "w") as wf:
+            sep = f"\n\n\n\n{'='*20}\n\n\n\n"
+            for txt in completed:
+                wf.write(txt + sep)
 
     """
     REDUCE the possible candidates we have less time!
@@ -155,6 +202,10 @@ def main():
     2. take good examples for correct (and wrong too)
 
     v3 p2c-cot pal-p2c pal-cot
+    math 1,2,3    1(weak)  1,2,3   
+    ocw  1        1,2,3     3
+
+
     # v2 cot-p2c pal-cot pal-p2c # for later use 
     # v1 cot-p2c pal-cot pal-p2c
 
--- src/prompt_construction_src/ocw_math_prep_rims_prompt/make_blurbs2prompt.py
+++ src/prompt_construction_src/ocw_math_prep_rims_prompt/make_blurbs2prompt.py
@@ -2,4 +2,119 @@
 1. rims prompt
 2. rims - hint prompt
 3. rims - (wrong + hint) prompt
+
+
+
+v3 p2c-cot pal-p2c pal-cot
+ocw  0        2     0,1,2   --> 0-2-1 only [one]
+math 1,2     aug    1,2     -->  2-aug-1 or 1-aug-2 [two]
+
+
+# spare blurb to augment above if needed:
+      cot-p2c
+ocw      2 (allows 1 variant for later if needed)
+math     0 (allows 3 variants for later)
 """
+
+import yaml
+from munch import munchify
+from fire import Fire
+
+from typing import Dict, List
+from collections import defaultdict
+from pathlib import Path
+
+
+def ablate(
+        blurb: str,
+    )->Dict[str,str]:
+    # find the end of attempt 1 = mistakes start 
+    attempt1_start = blurb.find ("`Attempt 1`: ")
+    attempt1_end = blurb.find("`Mistakes`: ")
+    
+    # find the end of mistakes = hint start
+    mistakes_start = attempt1_end
+    mistakes_end = blurb.find("`Hint for a better Method choice`: ")    
+    
+    # find the end of hint = attempt 2 start ( whole reflection ablated)
+    hint_start = mistakes_end 
+    hint_end = blurb.find("`Workaround Method`: ")
+    
+
+    raise NotImplementedError("Ablation not implemented yet")
+
+    ablations = {
+        "-hint":, 
+        "-hint-mistakes":,
+        "-hint-mistakes-attempt1":, 
+    }
+    return ablations
+    
+    
+     
+    
+
+
+
+def construct_prompt(
+        template_d: Dict[str,str] = None, 
+        blurbs_d: Dict[str, List[str]] = None, 
+        dirmap: dict = None, 
+        blurb_order: List[str] = None
+        ) -> str:
+    """
+    Construct the prompt using the template and blurbs
+    """
+    # get the template
+    system = template_d.system
+    # get the blurbs
+    blurbs = []
+    for direction in blurb_order:
+        current_dir_idx = dirmap[direction]
+        current_blurb = blurbs_d[current_dir_idx]
+        blurbs.append(current_blurb)
+    sep = template_d.sep
+    
+    inst = template_d.instruction 
+
+    all_parts_in_order = [system] + blurbs + [inst]
+    
+    rims_prompt = sep.join(all_parts_in_order)
+    return rims_prompt
+
+
+def main():
+    # predefined mappings of blurbs to use 
+    columns = "p2c-cot pal-p2c pal-cot".split()
+    prompt_mappings: Dict[List] = {
+        "ocw": [
+                dict(zip(columns, [0,2,1]))
+                ],
+        "math": [
+                dict(zip(columns, [2,0,1])), 
+                dict(zip(columns, [1,0,2])),
+                ]
+    }
+
+    # load blurbs
+    blurbs_d = yaml.full_load(open("math_ocw_selected_blurbs.yaml"))
+    blurbs_d = munchify(blurbs_d)
+
+    # load template
+    template_d = yaml.full_load(open("make_blurbs2prompt.yaml"))
+    template_d = munchify(template_d)
+    
+    # make the prompt
+    for ds, dirmappings_list in prompt_mappings.items():
+        for dirmap in dirmappings_list: 
+            prompt: str = construct_prompt(template_d, blurbs_d[ds], dirmap, blurb_order = columns)    
+            outf = f"rims_{ds}_{'.'.join(columns)}.txt"
+            if Path(outf).exists():
+                outf = outf.replace(".txt", ".txt1")
+            with open(outf, "w") as writer:
+                writer.write(prompt)
+                print(outf)
+
+
+if __name__ == "__main__":
+    Fire(main)

e3413ab0d7da286d705c025986a46fb00e2680b8
strutive07 <strutive07@gmail.com>
2024-06-06 22:36:39 +0900

    remove ipynb

--- src/0_refactoring/query/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,5 +0,0 @@
-from .base_query import BaseQueryObject
-from .query_utils import get_user_assistant_messages
-from .cot import CoTQueryObject
-from .pal import PALQueryObject
-from .p2c import P2CQueryObject

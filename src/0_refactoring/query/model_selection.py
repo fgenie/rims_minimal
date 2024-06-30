6e17251d98631a8231aa1b7b1455d3d32c684ad4
freeman.genie777@gmail.com <sonsus@github.com>
2024-06-30 16:11:33 +0900

    WIP model selection

---
+++ src/0_refactoring/query/model_selection.py
@@ -0,0 +1,67 @@
+"""
+code for model selection (i.e. simple-greedy)
+"""
+
+
+class ModelSelectionQuery(BaseQueryObject):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    async def prepare_query(
+        self,
+        question: str,
+        cot_pal_p2c_sln_d: dict,
+    ):
+        return get_select_prompt2(
+            question,
+            cot_pal_p2c_sln_d=cot_pal_p2c_sln_d,
+            dataset_type=self.dataset_type,
+        )
+
+    def query_error_msg(self, query_message):
+        return False, None
+
+
+### getting prompts for each method ###
+def get_select_prompt2(
+    question: str,
+    cot_pal_p2c_sln_d: dict = None,
+    dataset_type: Literal["gsm", "svamp", "ocw", "math"] = None
+    # backbone:str="chatgpt",
+) -> List[Dict[str, str]]:
+    # open up prompt template yaml file
+    prompt_yml = THIS_PARENT / "model_selection_prompts.yaml"
+    prompt_d: Dict[str, Any] = yaml.full_load(open(prompt_yml))
+
+    select_prompt_key = f"{dataset_type}_select"
+    ds_prom_d: Dict[str, Any] = prompt_d[select_prompt_key]
+
+    # process with the question/solutions of interest to result in gpt_messages
+    system: str = ds_prom_d["system"]
+    fewshots_user = ds_prom_d["user"]
+    fewshots_assistant = ds_prom_d["assistant"]
+
+    # make user's query from (question, cot_pal_p2c_sln_d)
+    q = question  # data['question']
+    to_replace_keys = "{COT_SOLUTION} {PAL_SOLUTION} {P2C_SOLUTION}"
+    user_tmp: str = ds_prom_d["user_tmp"]
+
+    # 1. fill the quesiton
+    user_tmp = user_tmp.replace("{QUESTION}", q)
+    # 2. fill the solutions
+    for to_replace, to_be in zip(to_replace_keys.split(), cot_pal_p2c_sln_d.values()):
+        user_tmp = user_tmp.replace(to_replace, to_be)
+    user_attempt = user_tmp
+
+    msgs: List[Dict[str, str]] = [
+        {"role": "system", "content": system},
+    ]
+    for fu, fa in zip(fewshots_user, fewshots_assistant):
+        usr = {"role": "user", "content": fu}
+        astnt = {"role": "assistant", "content": fa}
+        msgs.append(usr)
+        msgs.append(astnt)
+
+    msgs.append({"role": "user", "content": user_attempt})
+
+    return msgs
